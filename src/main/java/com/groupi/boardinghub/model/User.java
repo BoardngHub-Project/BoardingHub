912d41b82e3b6710ed5eb6f6d150199207816fdf
Kanchana <thennakoon.kanchana@gmail.com>
2023-09-23 22:15:30 +0530

    first commit

---
+++ src/main/java/com/groupi/boardinghub/model/User.java
@@ -0,0 +1,28 @@
+package com.groupi.boardinghub.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="user")
+public class User {
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2",strategy="uuid2")
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+}
